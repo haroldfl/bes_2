a36b9d95cdaf7a3f79fcf16261672e4518c9580e
DErnstl <ic17b015@technikum-wien.at>
2018-04-27 10:21:34 +0200

    27.04.2018: Child Prozess ls einlesen, über die Pipe an den Parent Prozess übergeben, dort die Pipe auslesen und ausgeben funktioniert. ZwischenAbgabe 1 done :)

--- popen.c
+++ popen.c
@@ -6,30 +6,71 @@
 
 FILE* mypopen(char* command, char* type){
     int fd[4];
-    pid_t pid;
+    pid_t *pid = NULL;
     char *befehl=malloc(1*sizeof(char*));
     int i=0;
+    char puffer[PIPE_BUF];
 
 
 
 
-
-
+    /* pipe:
+     * fd[0] reading end of pipe
+     * fd[1] writing end of pipe
+     * Error -1 and errno is set
+    */
     if(pipe(fd)<0){
         perror("pipe");
         exit(EXIT_FAILURE);
     }
+    /* fork:
+    * parent: >0
+    * child == 0
+    * Error -1 and errno is set
+    */
     if((pid=fork())<0){
         perror("fork");
         exit(EXIT_FAILURE);
     }
-    if(pid==0){
+    //PARENT
+    if(pid>0) {
+        if((strcmp(type,"r"))==0){
+            //read --> schreibe seite kann geschlossen werden
+            close(fd[1]);
+            //parent warten auf child
+            wait(pid);
+
+            //dupliziere pipe read der pipe auf stdin
+            if(read(fd[0],puffer,PIPE_BUF)==-1){
+                perror("read");
+                exit(EXIT_FAILURE);
+            }
+            printf("%s",puffer);
+
+            exit(0);
+
+        }
+
+    }
+        //CHILD
+    else if(pid==0){
         if((strcmp(type,"r"))==0) {
             close(fd[0]);
-           // dup2(,STDOUT_FILENO);
+            //dupliziere pipe write in stdout, sprich alles was auf stdout geht, geht in die pipe
+            if(dup2(fd[1],STDOUT_FILENO)==-1){
+                perror("dup2");
+                exit(EXIT_FAILURE);
+            }
+            //ausführen des Kommandos
+            if(execl("/bin/ls","ls","/home/ernstl/FH", NULL)==-1){
+                perror("\nError EXECL");
+                exit(EXIT_FAILURE);
+            }
+            printf("TEST");
+            exit(0);
         }else if((strcmp(type,"w"))==0){
-            close(fd[1]);
-
+            printf("write");
+            exit(0);
         }
     }
 
