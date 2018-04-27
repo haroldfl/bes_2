377417baab46fb09a76a09870b61d675d2a13d1d
DErnstl <ic17b015@technikum-wien.at>
2018-04-27 16:03:51 +0200

    27.04.2018: Child Prozess ls einlesen, über die Pipe an den Parent Prozess übergeben, dort die Pipe auslesen und ausgeben funktioniert. ZwischenAbgabe 1 done :)
    27.04.2018:
    Leicht erweitert das ganze

--- popen.c
+++ popen.c
@@ -3,16 +3,15 @@
 //
 #include "popen.h"
 
+pid_t child_pid;
 
 FILE* mypopen(char* command, char* type){
     int fd[4];
+    FILE* rfile = NULL;
     pid_t *pid = NULL;
     char *befehl=malloc(1*sizeof(char*));
     int i=0;
-    char puffer[PIPE_BUF];
-
-
-
+    char puffer[PIPE_BUF]; //PIPE_BUF max größe des buffers
 
     /* pipe:
      * fd[0] reading end of pipe
@@ -40,20 +39,55 @@ FILE* mypopen(char* command, char* type){
             //parent warten auf child
             wait(pid);
 
+            //############ --------------------------------------------- ##############
+            //############ zum Testzweck, Ausgabe der eingelesenen Datei ##############
             //dupliziere pipe read der pipe auf stdin
             if(read(fd[0],puffer,PIPE_BUF)==-1){
                 perror("read");
                 exit(EXIT_FAILURE);
             }
             printf("%s",puffer);
+            //############ --------------------------------------------- ##############
+
+            rfile = fdopen(fd[0],"r");
+            if(rfile == NULL){
+                perror("fdopen");
+                exit(EXIT_FAILURE);
+            }
+            return rfile;
 
             exit(0);
+        }
+        else if((strcmp(type,"w"))==0){
+            //read --> schreibe seite kann geschlossen werden
+            close(fd[0]);
+            //parent warten auf child
+            wait(pid);
 
+            //############ --------------------------------------------- ##############
+            //############ zum Testzweck, Ausgabe der eingelesenen Datei ##############
+            //dupliziere pipe read der pipe auf stdin
+            if(read(fd[1],puffer,PIPE_BUF)==-1){
+                perror("read");
+                exit(EXIT_FAILURE);
+            }
+            printf("%s",puffer);
+            //############ --------------------------------------------- ##############
+
+            rfile = fdopen(fd[1],"w");
+            if(rfile == NULL){
+                perror("fdopen");
+                exit(EXIT_FAILURE);
+            }
+            return rfile;
+
+            exit(0);
         }
 
     }
         //CHILD
     else if(pid==0){
+        child_pid = getpid();
         if((strcmp(type,"r"))==0) {
             close(fd[0]);
             //dupliziere pipe write in stdout, sprich alles was auf stdout geht, geht in die pipe
@@ -62,16 +96,39 @@ FILE* mypopen(char* command, char* type){
                 exit(EXIT_FAILURE);
             }
             //ausführen des Kommandos
-            if(execl("/bin/ls","ls","/home/ernstl/FH", NULL)==-1){
+            if(execl("/bin/ls",command, NULL)==-1){
                 perror("\nError EXECL");
                 exit(EXIT_FAILURE);
             }
             printf("TEST");
             exit(0);
-        }else if((strcmp(type,"w"))==0){
-            printf("write");
+        }else if((strcmp(type,"w"))==0) {
+            close(fd[1]);
+            //dupliziere pipe write in stdout, sprich alles was auf stdout geht, geht in die pipe
+            if(dup2(fd[0],STDIN_FILENO)==-1){
+                perror("dup2");
+                exit(EXIT_FAILURE);
+            }
+            //ausführen des Kommandos
+            if(execl("/bin/sh","sh","-c",command,NULL==-1)){
+                perror("\nError EXECL");
+                exit(EXIT_FAILURE);
+            }
+            printf("TEST");
             exit(0);
         }
     }
 
+}
+
+int mypclose(FILE *stream){
+    if(stream == NULL){
+        //errno = EINVAL;
+        return -1;
+    }
+    if(waitpid(child_pid, NULL,0)==-1){
+        return -1;
+    }
+    fclose(stream);
+    exit(EXIT_SUCCESS);
 }
